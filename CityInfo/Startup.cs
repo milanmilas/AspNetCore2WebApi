e223de5f4c4ce5da2f605a0810957de3e3089962
Milan Milas <milanmilas@gmail.com>
2018-04-30 19:53:27 +0100

    configuration fix

--- CityInfo/Startup.cs
+++ CityInfo/Startup.cs
@@ -22,7 +22,7 @@ namespace CityInfo
             Configuration = configuration;
         }
 
-        public static IConfiguration Configuration;
+        public static IConfiguration Configuration { get; private set;}
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
