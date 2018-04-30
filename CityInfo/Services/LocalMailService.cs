98f95d63f89e7a1be59f514135f466588a5657d8
Milan Milas <milanmilas@gmail.com>
2018-04-30 19:46:47 +0100

    configuration

--- CityInfo/Services/LocalMailService.cs
+++ CityInfo/Services/LocalMailService.cs
@@ -9,8 +9,8 @@ namespace CityInfo.API.Services
 {
     public class LocalMailService : IMailService
     {
-        private string _mailTo = "admin@mycompany.com";
-        private string _mailFrom = "noreply@mycompany.com";
+        private string _mailTo = Startup.Configuration["mailSettings:mailToAddress"];
+        private string _mailFrom = Startup.Configuration["mailSettings:mailFromAddress"];
 
         public void Send(string subject, string message){
             //send mail - output to debug window
